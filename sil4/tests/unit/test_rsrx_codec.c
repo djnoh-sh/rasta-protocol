646226f7b73400ebae3876b97528ba50cba6ec20
DJ Noh <djnoh@sehwa.biz>
2026-05-21 09:40:02 +0900

    test: cover trailing bytes decode clear

--- sil4/tests/unit/test_rsrx_codec.c
+++ sil4/tests/unit/test_rsrx_codec.c
@@ -741,7 +741,9 @@ static void vTestDecodeRejectsTrailingBytes(void)
 	xFrame.xPayloadLength = sizeof(auEncoded);
 	xFrame.eEventType = RSRX_TRANSPORT_EVENT_FRAME_RECEIVED;
 
+	vSeedDecodedMessage(&xMessage);
 	vAssertTrue(rsrx_codec_decode_frame(&xFrame, &xMessage) == RSRX_CODEC_STATUS_TRAILING_BYTES, "trailing byte reject");
+	vAssertDecodedMessageCleared(&xMessage, "trailing byte clears stale decoded message");
 }
 
 static void vTestDecodeRejectsShortHeader(void)
